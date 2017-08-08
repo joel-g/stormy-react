598973c9e5d644e0c6d66d1cdafebbed99588482
joel-g <joelaguerra1@gmail.com>
2017-08-07 21:19:08 -0700

    completes skeleton

---
+++ src/about.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import PropTypes from 'proptypes';
+
+function About() {
+
+}
--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import PropTypes from 'proptypes';
 
-function Nav() {
+function Contact() {
 
 }

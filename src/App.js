de0618d5f53b34505d9babfd6a1d694a62306ba9
joel-g <joelaguerra1@gmail.com>
2017-08-07 21:06:06 -0700

    imports files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Home from './home';
+import Services from './services';
+import About from './about';
+import Contact from '.contact';
 import './App.css';
+
 
 class App extends Component {
   render() {

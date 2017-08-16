6b290cec25d7eb157f450492d090183fd3fe67fe
Joel Guerra <joelaguerra1@gmail.com>
2017-08-15 21:23:03 -0700

    builds services page

--- src/services.js
+++ src/services.js
@@ -4,7 +4,17 @@ import React from 'react';
 function Services() {
   return (
     <div className='content'>
-      SERVICES PAGE
+      <ul className='services-list'>
+        <li>Legal compliance</li>
+        <li>Staff training</li>
+        <li>Staff hiring</li>
+        <li>Tribal relations</li>
+        <li>Cultivation</li>
+        <li>Genetics selection</li>
+        <li>Branding</li>
+        <li>Marketing</li>
+      </ul>
+      (each of these will be a link that describes the topic in depth)
     </div>
   )
 }

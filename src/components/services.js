d5a55e2328702f459ba28c31703ed3890d561e37
Joel Guerra <joelaguerra1@gmail.com>
2017-08-16 11:19:33 -0700

    working click Services click handler

--- src/components/services.js
+++ src/components/services.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 // import PropTypes from 'proptypes';
 
 class Services extends Component {
   constructor() {
     super();
     this.state = {
-      currentDisplay: 'Legal compliance'
+      currentDisplay: 'Staff training'
     }
   }
+
+  servicesClickHandler(e){
+    this.setState({currentDisplay: $(e.target).text()})
+  }
+
   render() {
     let display;
     if (this.state.currentDisplay === 'Legal compliance') {
@@ -18,22 +24,30 @@ class Services extends Component {
       display = <p>staff hiring description</p>
     } else if (this.state.currentDisplay === 'Tribal relations') {
       display = <p>tribal relations description</p>
+    } else if (this.state.currentDisplay === 'Cultivation') {
+      display = <p>Cultivation description</p>
+    } else if (this.state.currentDisplay === 'Genetics selection') {
+      display = <p>Genetics description</p>
+    } else if (this.state.currentDisplay === 'Branding') {
+      display = <p>Branding description</p>
+    } else if (this.state.currentDisplay === 'Marketing') {
+      display = <p>Marketing description</p>
     };
     return (
       <div className='content'>
         <ul className='services-list'>
-          <li>Legal compliance</li>
-          <li>Staff training</li>
-          <li>Staff hiring</li>
-          <li>Tribal relations</li>
-          <li>Cultivation</li>
-          <li>Genetics selection</li>
-          <li>Branding</li>
-          <li>Marketing</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Legal compliance</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Staff training</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Staff hiring</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Tribal relations</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Cultivation</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Genetics selection</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Branding</li>
+          <li onClick={ e =>this.servicesClickHandler(e) }>Marketing</li>
         </ul>
-      <div>
-        {display}
-      </div>
+        <div className='service-description'>
+          {display}
+        </div>
       </div>
     )
   }

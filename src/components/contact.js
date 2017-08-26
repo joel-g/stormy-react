a4f91686d3b928ea63ba5979d091e45f4e2d3146
joel-g <joelaguerra1@gmail.com>
2017-08-26 10:52:43 -0700

    contact form works

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,10 +6,12 @@ function Contact() {
     <Fade duration={0.5}>
     <div className='content'>
       <h1 className='page-header'>Contact Me</h1>
-      <form className='contact-form' action='' method='POST'>
-      <input type='text' placeholder='Business name'></input>
+      <form className='contact-form' action='https://formspree.io/joelaguerra1@gmail.com' method='POST'>
+      <input type='text' placeholder='Your name' name='person'></input>
+      <input type='text' placeholder='Title' name='title'></input><br />
+      <input type='text' placeholder='Business name' name='business'></input>
       <select>
-        <option defaultValue="unspecified">Business type:</option>
+        <option defaultValue="unspecified" name='business-type'>Business type:</option>
         <option value="retail">Retail</option>
         <option value="medical">Medical</option>
         <option value="producer">Producer</option>
@@ -17,8 +19,6 @@ function Contact() {
       </select><br />
       <input type='text' placeholder='City' name='city'></input>
       <input type='text' placeholder='State' name='state'></input><br />
-      <input type='text' placeholder='Contact person' name='contact'></input>
-      <input type='text' placeholder='Contact person title' name='title'></input><br />
       <button type='submit'>Submit</button>
       </form>
       <p>{"This doesn't work yet."}</p>

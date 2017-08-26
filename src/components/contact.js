f58f62a1b618620479711d289afc5506f4c884a9
joel-g <joelaguerra1@gmail.com>
2017-08-26 11:49:15 -0700

    styled form and made about more responsive

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,12 +6,14 @@ function Contact() {
     <Fade duration={0.5}>
     <div className='content'>
       <h1 className='page-header'>Contact Me</h1>
-      <form className='contact-form' action='https://formspree.io/joelaguerra1@gmail.com' method='POST'>
-      <input type='text' placeholder='Your name' name='person'></input>
-      <input type='text' placeholder='Title' name='title'></input><br />
+      <form className='contact-form' action='https://formspree.io/stormyguerra@gmail.com' method='POST'>
+      <input type='text' placeholder='Your name' name='person' />
+      <input type='text' placeholder='Title' name='title' /><br />
+      <input type='text' placeholder='Email' name='_replyto' />
+      <input type='text' placeholder='Phone #' name='phone' /><br />
       <input type='text' placeholder='Business name' name='business'></input>
-      <select>
-        <option defaultValue="unspecified" name='business-type'>Business type:</option>
+      <select className='dropdown'>
+        <option  defaultValue="unspecified" name='business-type'>Business type:</option>
         <option value="retail">Retail</option>
         <option value="medical">Medical</option>
         <option value="producer">Producer</option>
@@ -19,9 +21,13 @@ function Contact() {
       </select><br />
       <input type='text' placeholder='City' name='city'></input>
       <input type='text' placeholder='State' name='state'></input><br />
-      <button type='submit'>Submit</button>
+      <input type="hidden" name="_next" value="http://stormyguerra.com" />
+      <input type="hidden" name="_subject" value="Stormguerra.com" />
+      <textarea className='message' name='message'>
+        How can I help?
+      </textarea><br />
+      <button type='submit' className='btn'>Submit</button>
       </form>
-      <p>{"This doesn't work yet."}</p>
     </div>
    </Fade>
   )

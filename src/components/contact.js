b38b325fc605cfc08e2a42b5eead5da7bda6d9d4
joel-g <joelaguerra1@gmail.com>
2017-08-27 01:13:31 -0700

    fully ajaxifies form

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,34 +9,46 @@ class Contact extends Component {
     super();
     this.state = {
       currentDisplay: 'Form',
-      errorMessage: ''
+      userMessage: '',
+      showForm: true
     }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-
   handleSubmit(e){
     e.preventDefault();
     $.ajax({
-        url: "https://formspree.io/joelaguerra1@gmail.com",
+        context: this,
+        url: "https://formspree.io/stormyguerra1@gmail.com",
         method: "POST",
         data: $('.contact-form').serialize(),
         dataType: "json"
-    }).done(function(response){
-      console.log('success', response)
-    }).fail(function(response){
-      console.log('error', response)
+    }).done(function(r){
+      $('.form-message').css('color', 'black')
+      this.setState({
+        userMessage: "Your message has been sent. Stormy responds to inquiries within 24 hours.",
+        showForm: false
+       })
+    }).fail(function(r){
+      $('.form-message').css('color', 'red')
+      this.setState({ userMessage: "Please enter a valid email address."})
     });
   }
 
 
-
   render() {
+    let form;
+    if (this.state.showForm){
+      form = <Form handleSubmit={this.handleSubmit} />
+    } else { form = '' }
     return (
       <Fade duration={0.5}>
       <div className='content'>
         <h1 className='page-header'>Contact Me</h1>
-        <Form handleSubmit={this.handleSubmit} />
+        <div className='form-message'>
+          {this.state.userMessage}
+        </div>
+        {form}
       </div>
      </Fade>
     )
